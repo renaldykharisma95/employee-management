59f24d9dfaab61e143219a679f0c085051c92cc5
renaldykharisma <renaldy@radyalabs.com>
2021-08-27 21:35:09 +0700

    observer update

--- src/app/utils/observers/observers-service.service.ts
+++ src/app/utils/observers/observers-service.service.ts
@@ -6,11 +6,16 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class ObserversServiceService {
 
+  arrCollectData:any [] = []
+
   private titlepage = new BehaviorSubject('');
   titlePage = this.titlepage.asObservable();
 
   private breadcrumb = new BehaviorSubject([]);
   breadCrumb = this.breadcrumb.asObservable();
+
+  private employeedatas = new BehaviorSubject([]);
+  employeedata = this.employeedatas.asObservable();
 
   setMenuTitle(name: string) {
     this.titlepage.next(name);
@@ -18,5 +23,26 @@ export class ObserversServiceService {
 
   setBreadcrumb(name: any) {
     this.breadcrumb.next(name);
+  }
+ 
+  setEmployeeData(data: any, type: number){
+    switch(type){
+      case 0:{
+        this.arrCollectData = [...this.arrCollectData, data];
+        this.employeedatas.next(this.arrCollectData);
+        break;
+      }
+      case 1:{
+        for (var i in this.arrCollectData) {
+          if (this.arrCollectData[i].id == data.id) {
+             this.arrCollectData[i] = data;
+             break;
+          }
+        }
+        this.employeedatas.next(this.arrCollectData);
+        break;
+      }
+    }
+    
   }
 }
