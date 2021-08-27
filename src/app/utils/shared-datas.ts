89311d9842c3c68f64f2013aef473081bd210f87
renaldykharisma <renaldy@radyalabs.com>
2021-08-27 21:34:41 +0700

    employee route data

--- src/app/utils/shared-datas.ts
+++ src/app/utils/shared-datas.ts
@@ -5,4 +5,69 @@ export const employeeListRoute = [
         link: '',
         name: 'Employee List'
     }
-]
+];
+
+export const employeeCreateRoute = [
+    {
+        link: '/employee-list',
+        name: 'Employee List'
+    },
+    {
+        link: '',
+        name: 'Employee Create'
+    }
+];
+
+export const employeeEditRoute = [
+    {
+        link: '/employee-list',
+        name: 'Employee List'
+    },
+    {
+        link: '',
+        name: 'Employee Edit'
+    }
+];
+
+export const groupList = [
+    {
+        id: 1,
+        name: 'group 1'
+    },
+    {
+        id: 2,
+        name: 'group 2'
+    },
+    {
+        id: 3,
+        name: 'group 3'
+    },
+    {
+        id: 4,
+        name: 'group 4'
+    },
+    {
+        id: 5,
+        name: 'group 5'
+    },
+    {
+        id: 6,
+        name: 'group 6'
+    },
+    {
+        id: 7,
+        name: 'group 7'
+    },
+    {
+        id: 8,
+        name: 'group 8'
+    },
+    {
+        id: 9,
+        name: 'group 9'
+    },
+    {
+        id: 10,
+        name: 'group 10'
+    }
+];
